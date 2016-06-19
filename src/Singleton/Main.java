9d591654560b4411bca6cf064155acfab54a4f4a
vT4K <pcity@MacBookAir.local>
2016-06-19 12:50:02 -0400

    Singleton and Builder

---
+++ src/Singleton/Main.java
@@ -0,0 +1,16 @@
+package Singleton;
+
+/**
+ * Created by pcarfrey on 6/19/16.
+ */
+public class Main {
+    public static void main(String arg[]) {
+
+        Singleton instance = Singleton.getInstance();
+        System.out.println(instance); //address in memory
+
+        Singleton anotherInstance = Singleton.getInstance();
+        System.out.println(anotherInstance); //will have same address in memory
+
+    }
+}
