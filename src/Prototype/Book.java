265ba7175a801f005feb9937108dc8106f1c5ddd
vT4K <pcity@MacBookAir.local>
2016-06-21 00:48:36 -0400

    Prototype

---
+++ src/Prototype/Book.java
@@ -0,0 +1,17 @@
+package Prototype;
+
+/**
+ * Created by pcarfrey on 6/21/16.
+ */
+public class Book extends Item {
+
+    private int numberOfPages;
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+}
---
+++ src/Prototype/Movie.java
@@ -0,0 +1,17 @@
+package Prototype;
+
+/**
+ * Created by pcarfrey on 6/21/16.
+ */
+public class Movie extends Item{
+
+    private String runtime;
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+}
