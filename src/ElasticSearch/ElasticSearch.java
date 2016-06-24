0c55fd78ff4ecae82730da7039f22abfec9876a3
Carfrey <patrick.carfrey@mheducation.com>
2016-06-24 10:16:04 -0400

    es snippet

---
+++ src/ElasticSearch/ElasticSearch.java
@@ -0,0 +1,23 @@
+/**
+ * Created by pcarfrey on 6/24/16.
+ */
+public class ElasticSearch {
+
+    Settings settings = Settings.settingsBuilder()
+            .put("cluster.name", "clusterNameHere").build();
+
+    Client client = TransportClient.builder()
+            .settings(settings)
+            .build()
+            .addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("127.0.0.1", 9300)));
+
+    SearchResponse response = client.prepareSearch("documentNameHere")
+            .setTypes("typeHere")
+            .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+            .setQuery(QueryBuilders.termQuery("key", "value")) // Query
+            .setPostFilter(QueryBuilders.rangeQuery("age").from(12).to(18))   // Filter
+            .setFrom(0).setSize(60).setExplain(true)
+            .execute()
+            .actionGet();
+    
+}
